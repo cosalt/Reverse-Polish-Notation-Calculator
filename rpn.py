a4f48740741398beae19ec465a673e4f1a1bd2ea
Nawin Ray Martin <91860754+cosalt@users.noreply.github.com>
2025-03-05 09:09:54 +0700

    Create rpn.py

---
+++ rpn.py
@@ -0,0 +1,19 @@
+def rpn(arr):
+    stack = [" " for i in range(len(arr))]
+    topofstack = -1
+    for count, number in enumerate(arr):
+
+        if number in ["*", "+", "-", "/"]:
+    
+            
+            num1 = str(stack[topofstack])
+            topofstack -= 1
+            num2 = str(stack[topofstack])
+            topofstack -= 1
+            sum = eval(num2 + number + num1)
+            topofstack += 1
+            stack[topofstack] = sum
+        else:
+            topofstack += 1
+            stack[topofstack] = number
+    return stack[topofstack]
