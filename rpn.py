503efa02db3b2a05dc713aafd3ace6351476dd8a
Nawin Ray Martin <91860754+cosalt@users.noreply.github.com>
2025-03-05 09:10:04 +0700

    Update rpn.py

--- rpn.py
+++ rpn.py
@@ -2,10 +2,7 @@ def rpn(arr):
     stack = [" " for i in range(len(arr))]
     topofstack = -1
     for count, number in enumerate(arr):
-
         if number in ["*", "+", "-", "/"]:
-    
-            
             num1 = str(stack[topofstack])
             topofstack -= 1
             num2 = str(stack[topofstack])
